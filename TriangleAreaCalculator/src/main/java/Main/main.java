496569901c6f82594088ba583580f56395c124d0
Isaque Fabro <isaquefabro04@gmail.com>
2024-11-05 11:43:58 -0200

    Projeto finalizado

---
+++ TriangleAreaCalculator/src/main/java/Main/main.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.util.Scanner;
+import model.Triangle;
+
+public class main {
+
+    public static void main(String[] args) {
+        // create scanner for user input
+        Scanner scanner = new Scanner(System.in);
+        // create a new Triangle object
+        Triangle triangle = new Triangle();
+        
+        // prints triangle object
+        System.out.println("Triangle: " + triangle); 
+
+        // asks for the base of the triangle
+        System.out.println("Enter the base: ");
+        triangle.setBase(scanner.nextDouble());
+
+        // asks for the height of the triangle
+        System.out.println("Enter the height: ");
+        triangle.setHeight(scanner.nextDouble());
+
+        // prints the area of the triangle
+        System.out.println("The area is: " + triangle.getArea());
+    }
+}
